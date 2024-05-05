54d041d7e82caff27013c4fd0d185398c273ef13
Jordan <jckail13@gmail.com>
2024-05-05 14:43:35 -0600

    added return token when authenticated

--- api/auth/privateEndpointExample.py
+++ api/auth/privateEndpointExample.py
@@ -1,8 +1,9 @@
 from fastapi import Depends, Response, status
 
 from fastapi.security import HTTPBearer
+from numpy import result_type
 
-from auth.utils import VerifyToken
+from auth.utils import Auth0Token, generateToken
 
 import fastapi
 
@@ -19,10 +20,29 @@ token_auth_scheme = HTTPBearer()
 def privateExample(response: Response, token: str = Depends(token_auth_scheme)):
     """A valid access token is required to access this route"""
 
-    result = VerifyToken(token.credentials).verify()
+    result = Auth0Token(jwt_access_token=token.credentials).verify()
 
     if result.get("status"):
         response.status_code = status.HTTP_400_BAD_REQUEST
         return result
 
     return result
+
+
+@router.post("/api/privateGen")  ## add userid to path
+def privateGen(permissions=None, scopes=None, token: str = Depends(token_auth_scheme)):
+    """A valid access token is required to access this route"""
+
+    result = Auth0Token(jwt_access_token=token.credentials).verify()
+
+    if result.get("status"):
+        # return unauthenticated
+        return status.HTTP_403_FORBIDDEN
+
+    return generateToken(permissions, scopes)
+
+    # # if result.get("status"):
+    # #     status= status.HTTP_400_BAD_REQUEST
+    # #     return result
+
+    # return {"response": status, "result": token}
--- auth/utils.py
+++ auth/utils.py
@@ -1,33 +1,49 @@
 import os
 import jwt
-
-
+import http.client
+import json
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from typing import Optional, List, Dict
+
+
+class ApiResponse(BaseModel):
+    success: bool
+    message: str
+    data: dict
 
 
 load_dotenv()
 
 
-class VerifyToken:
+class AuthToken(BaseModel):
+    access_token: str
+    scope: str
+    expires_in: int
+    token_type: str
+
+
+class Auth0Token(BaseModel):
     """Does all the token verification using PyJWT"""
 
-    def __init__(self, jwt_access_token: str, permissions=None, scopes=None):
-        self.jwt_access_token = jwt_access_token
-        self.permissions = permissions
-        self.scopes = scopes
+    jwt_access_token: str
+    permissions: Optional[List[str]] = None
+    scopes: Optional[List[str]] = None
 
-        # This gets the JWKS from a given URL and does processing so you can use any of
-        # the keys available
-        self.auth0_issuer_url: str = f"https://{str(os.getenv('AUTH0_DOMAIN'))}/"
-        self.auth0_audience: str = str(os.getenv("AUTH0_AUDIENCE"))
-        self.algorithm: str = "RS256"
-        self.jwks_uri: str = f"{self.auth0_issuer_url}.well-known/jwks.json"
-        self.jwks_client = jwt.PyJWKClient(self.jwks_uri)
+    # These are environment-dependent and should be obtained outside of the Pydantic model,
+    # or default values can be used directly within the model definition.
+    auth0_domain: str = os.getenv("AUTH0_DOMAIN")
+    auth0_issuer_url: str = f"https://{auth0_domain}/"
+    auth0_audience: str = os.getenv("AUTH0_AUDIENCE")
+    algorithm: str = "RS256"
+    jwks_uri: str = f"https://{auth0_domain}/.well-known/jwks.json"
+    payload: Optional[Dict] = None
 
     def verify(self):
         # This gets the 'kid' from the passed token
         try:
-            self.signing_key = self.jwks_client.get_signing_key_from_jwt(
+            jwks_client = jwt.PyJWKClient(self.jwks_uri)
+            signing_key = jwks_client.get_signing_key_from_jwt(
                 self.jwt_access_token
             ).key
         except jwt.exceptions.PyJWKClientError as error:
@@ -38,7 +54,7 @@ class VerifyToken:
         try:
             payload = jwt.decode(
                 self.jwt_access_token,
-                self.jwks_client.get_signing_key_from_jwt(self.jwt_access_token).key,
+                signing_key,
                 algorithms=self.algorithm,
                 audience=self.auth0_audience,
                 issuer=self.auth0_issuer_url,
@@ -88,3 +104,73 @@ class VerifyToken:
                 )
                 return result
         return result
+
+
+# def generateToken(permissions=None, scopes=None, payload=None):
+
+#     token = None
+#     res = None
+
+#     try:
+#         conn = http.client.HTTPSConnection(str(os.getenv("AUTH0_DOMAIN")))
+#         # Correctly formatted payload string with placeholders
+#         # payload = '{{"client_id":"{client_id}","client_secret":"{client_secret}","audience":"{audience}","grant_type":"client_credentials","permissions":"{permissions}"}}'
+#         # payload = payload.format(
+#         #     client_id=os.getenv("AUTH0_CLIENT_ID"),
+#         #     client_secret=os.getenv("AUTH0_CLIENT_SECRET"),
+#         #     audience=str(os.getenv("AUTH0_AUDIENCE")),
+#         #     permissions=permissions,
+#         #     payload=payload,
+#         # )
+#         payload = {
+#             "client_id": os.getenv("AUTH0_CLIENT_ID"),
+#             "client_secret": os.getenv("AUTH0_CLIENT_SECRET"),
+#             "audience": str(os.getenv("AUTH0_AUDIENCE")),
+#             "grant_type": "client_credentials",
+#         }
+
+#         # todo add in scopes, permissions, and user
+#         headers = {"content-type": "application/json"}
+#         conn.request("POST", "/oauth/token", payload, headers)
+#         res = conn.getresponse()
+
+
+#         if res.status == 200:
+#             data = res.read()
+#             token = data.decode("utf-8")
+#             return res.status, token
+#         else:
+#             return None
+#     except Exception as e:
+#         raise e
+
+
+def generateToken(permissions=None, scopes=None):
+    try:
+        conn = http.client.HTTPSConnection(os.getenv("AUTH0_DOMAIN"))
+        payload = {
+            "client_id": os.getenv("AUTH0_CLIENT_ID"),
+            "client_secret": os.getenv("AUTH0_CLIENT_SECRET"),
+            "audience": os.getenv("AUTH0_AUDIENCE"),
+            "grant_type": "client_credentials",
+        }
+
+        # Include permissions and scopes if provided
+        if permissions:
+            payload["permissions"] = permissions
+        if scopes:
+            payload["scope"] = " ".join(
+                scopes
+            )  # Assuming scopes are to be space-separated
+
+        headers = {"Content-Type": "application/json"}
+        payload_json = json.dumps(payload)
+        conn.request("POST", "/oauth/token", payload_json, headers)
+        res = conn.getresponse()
+        if res.status == 200:
+            data_dict = json.loads(res.read())
+            return AuthToken(**data_dict)
+        else:
+            return res
+    except Exception as e:
+        raise e
